3ec619ce87bf6887fbea62797cf71cf46a9fe130
Maldu <anamld62@gmail.com>
2025-03-21 12:00:10 +0100

    binary search script added

---
+++ algorithms/binary_search.py
@@ -0,0 +1,34 @@
+def binary_search(list,target):
+    """first and last variables refer to value of the indexes of the list
+    """
+    first = 0
+    last = len(list) - 1
+    
+    while first <= last:
+        midpoint = (first + last) //2 # "//" rounds to the unit
+        
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+            
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not found in list")
+        
+# IMPORTANT! THIS ALGORITHM NEEDS AN ORDERED LIST
+
+numbers = [1,2,3,4,5,6,7,8,9,19]
+
+result = binary_search(numbers,12)
+verify(result)
+
+result = binary_search(numbers,6)
+verify(result)
+            
