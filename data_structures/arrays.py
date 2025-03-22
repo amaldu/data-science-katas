3b517f79c2d28e676780841ad6d8837e2f37891a
Maldu <anamld62@gmail.com>
2025-03-22 09:15:35 +0100

    array and linked lists cripts added

---
+++ data_structures/arrays.py
@@ -0,0 +1,18 @@
+new_list = [1,2,3] # in python when we mean an array we mean a list
+
+result = new_list[0]
+
+
+if 1 in new_list:print(True)
+
+for n in new_list:
+    if n ==1:
+        print(True)
+        
+
+numbers = []
+len(numbers)
+numbers.append(2)
+numbers.append(200)
+
+len(numbers)
